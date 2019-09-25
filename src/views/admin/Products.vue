232e1ef394fda096d1418573e6c2f8bc3722c2f9
samuel.lefevr@gmail.com <samu@Mac-mini-de-Samuel.local>
2019-09-25 14:17:34 +0200

    main pages

---
+++ src/views/admin/Products.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <ProductList></ProductList>
+  </div>
+</template>
+
+<script>
+import ProductList from "@/components/admin/ProductList";
+
+export default {
+  components: {
+    ProductList
+  }
+};
+</script>
+
+<style scoped></style>
